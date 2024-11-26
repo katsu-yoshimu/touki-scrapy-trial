b87f2ce097e04b64fbdc86475e327cf6511422d8
katsu-yoshimu <ka.yoshimu@gmail.com>
2024-11-26 14:22:15 +0900

    tkinterを無効化

--- Message.py
+++ Message.py
@@ -1,16 +1,18 @@
 # 確認ダイアログ
-from tkinter import Tk,messagebox
+# from tkinter import Tk,messagebox
 
 
 def MessageForefront(MesAlarm):
-    root = Tk()
-    root.attributes('-topmost', True)
-    root.withdraw()
-    ret = messagebox.askyesno('確認', MesAlarm)
-    return ret
+    # root = Tk()
+    # root.attributes('-topmost', True)
+    # root.withdraw()
+    # ret = messagebox.askyesno('確認', MesAlarm)
+    # return ret
+    return True
 
 def MessageForefrontShowinfo(MesAlarmShowinfo):
-    root = Tk()
-    root.attributes('-topmost', True)
-    root.withdraw()
-    messagebox.showinfo('確認', MesAlarmShowinfo)
+    # root = Tk()
+    # root.attributes('-topmost', True)
+    # root.withdraw()
+    # messagebox.showinfo('確認', MesAlarmShowinfo)
+    pass
